a739aef215943deca293f7265e484717b41c2ce4
VolkanAppAkademie <supafly_69@gmx.de>
2025-01-21 12:13:26 +0100

    inserted assets

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,28 @@ class SnackScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/hintergründe/bg_startscreen.png',
+            fit: BoxFit.cover,
+          ),
+          Image.asset(
+            width: 1000,
+            height: 500,
+            'assets/grafiken/cupcake_chick.png',
+            //fit: BoxFit.cover,
+          ),
+        ],
+      ),
+
+      /*Image.asset(
         'assets/hintergründe/bg_startscreen.png',
         fit: BoxFit.cover,
         width: double.infinity,
         height: double.infinity,
-      ),
+      ),*/
     );
   }
 }
